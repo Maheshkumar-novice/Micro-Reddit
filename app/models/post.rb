da2e70230764ed703305a625b9c6c0312d7fba49
Maheshkumar <callmeelprofessor@gmail.com>
2022-04-12 09:19:42 +0530

    Create association between user and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :title, :url, :user_id, presence: true
   validates :title, length: { in: 4..20 }
   validates :url, length: { in: 4..40 }
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :username, :email, uniqueness: true
   validates :username, length: { in: 4..12 }
   validates :password, length: { in: 8..16 }
+
+  has_many :posts
 end
